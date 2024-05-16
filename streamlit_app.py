dee76894af5ad7ee65a5b57702409ddc46059a52
GrandHussar <102661166+GrandHussar@users.noreply.github.com>
2024-05-16 13:07:36 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,6 @@ if file is None:
     st.text("Please upload an image file")
 else:
     image=Image.open(file)
-    image = np.array(image.convert("RGB"))
     st.image(image,use_column_width=True)
     prediction=import_and_predict(image,model)
     class_names=['1','2','3','4','5','6','7','8','9','0']
